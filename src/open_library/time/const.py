07c3cc80ede11b49c03b23ee5534a0c525551566
eugenekim <eugenekim@macs-MacBook-Pro.local>
2023-12-19 09:48:49 +0900

    wip

--- src/open_library/time/const.py
+++ src/open_library/time/const.py
@@ -14,10 +14,17 @@ class TimeUnit(str, Enum):
 
 
 class TimeFrame(Enum):
+    SECOND_1 = timedelta(seconds=1)
+    SECOND_5 = timedelta(seconds=5)
+
+    SECOND_30 = timedelta(seconds=30)
+
     MINUTE_1 = timedelta(minutes=1)
     MINUTE_3 = timedelta(minutes=3)
     MINUTE_5 = timedelta(minutes=5)
+    MINUTE_10 = timedelta(minutes=10)
     HOUR_1 = timedelta(hours=1)
     DAY_1 = timedelta(days=1)
+    WEEK_1 = timedelta(days=7)
 
     UNDEFINED = None

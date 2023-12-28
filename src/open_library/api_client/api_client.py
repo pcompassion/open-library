d1b6ad0b6cba36e8dd54e3fec45ede3ae05d03f7
littlehome <littlehome@littlehomeui-MacBookAir.local>
2023-12-28 14:52:30 +0900

    cleanup

--- src/open_library/api_client/api_client.py
+++ src/open_library/api_client/api_client.py
@@ -11,6 +11,12 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+httpx_logger = logging.getLogger("httpx")
+httpx_logger.setLevel(logging.WARNING)
+
+httpcore_logger = logging.getLogger("httpcore")
+httpcore_logger.setLevel(logging.WARNING)
+
 
 class TokenManager:
     # periodic proactive token refreshing
@@ -38,8 +44,8 @@ class TokenManager:
                 periodic_wrapper(60 * PERIOD_CHECK_MIN, self.refresh_token)
             )
 
-    async def get_access_token(self) -> Optional[str]:
-        if self.is_near_expiration():
+    async def get_access_token(self, force_refresh=False) -> Optional[str]:
+        if self.is_near_expiration() or force_refresh:
             await self.refresh_token()
         return self.access_token
 
@@ -93,6 +99,7 @@ class ApiClient:
         client_id: str,
         client_secret: str,
         min_interval_sec: float = 0.5,
+        should_refresh_token_func: Callable[[httpx.Response], bool] | None = None,
     ):
         self.client = httpx.AsyncClient(verify=False)
         self.token_manager = TokenManager(
@@ -105,10 +112,11 @@ class ApiClient:
         self.min_interval_sec = min_interval_sec
 
         self.last_request_time = None
+        self.should_refresh_token_func = should_refresh_token_func
 
     async def request(
         self, url: str, method: str = "GET", **kwargs: Any
-    ) -> requests.Response:
+    ) -> httpx.Response:
         # Proactive token refreshing
         if self.token_manager.is_near_expiration():
             await self.token_manager.refresh_token()
@@ -119,26 +127,19 @@ class ApiClient:
         headers["Authorization"] = f"Bearer {access_token}"
         kwargs["headers"] = headers
 
-        if self.min_interval_sec and self.last_request_time is not None:
-            elapsed = time.monotonic() - self.last_request_time
-            if elapsed < self.min_interval_sec:
-                await asyncio.sleep(self.min_interval_sec - elapsed)
-
         response = await self.client.request(method, url, **kwargs)
-        self.last_request_time = time.monotonic()
 
         # Reactive token refreshing
-        data = response.json()
-        rsp_cd = data["rsp_cd"]
         if response.status_code != 200:
             logger.warning(f"no good {response.status_code}")
 
-            if response.status_code == 401 or (
-                response.status_code == 500 and rsp_cd.startswith("IGW00")
+            if (
+                self.should_refresh_token_func is not None
+                and self.should_refresh_token_func(response)
             ):
-                logger.info(f"refresh token {rsp_cd} {response.status_code}")
-
-                access_token = await self.token_manager.get_access_token()
+                access_token = await self.token_manager.get_access_token(
+                    force_refresh=True
+                )
                 headers["Authorization"] = f"Bearer {access_token}"
                 response = await self.client.request(method, url, **kwargs)
 
@@ -152,7 +153,7 @@ class ApiResponse:
     def __init__(
         self,
         success,
-        raw_data=None,
+        raw_data,
         data_field_name=None,
         error_code=None,
         default_data_type=list,

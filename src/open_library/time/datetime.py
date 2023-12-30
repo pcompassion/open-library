609d3f8d8cd898d354d0468465724098fbf2da03
eugenekim <eugenekim@macs-MacBook-Pro.local>
2023-12-30 10:18:43 +0900

    f

--- src/open_library/time/datetime.py
+++ src/open_library/time/datetime.py
@@ -2,6 +2,28 @@
 import pendulum
 
 
+# Function to determine the date based on the time and base_date
+def determine_datetime(time_str, base_datetime, time_format):
+    hour = int(time_str[:2])
+    if hour > 23:
+        hour = hour - 24
+        time_str = f"{hour:02d}{time_str[2:]}"
+
+    time = pendulum.from_format(time_str, time_format).time()
+    date = None
+    base_date = base_datetime.date()
+    base_time = base_datetime.time()
+
+    if time < base_time:
+        date = base_date
+    else:
+        date = base_date.subtract(days=1)
+    datetime = pendulum.DateTime.combine(date, time).in_tz(base_datetime.tzinfo)
+    datetime_str = datetime.to_iso8601_string()
+    native_datetime = datetime.fromisoformat(datetime_str)
+    return native_datetime
+
+
 def intervals_overlap(interval1, interval2):
     start1, end1 = interval1
     start2, end2 = interval2
